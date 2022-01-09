97369235ee94403dcd989114529dcbcb6a3782e6
mdhasanali3 <43574156+mdhasanali3@users.noreply.github.com>
2022-01-09 13:48:29 +0600

    running  server and client concurrently

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express= require('express')
+const app= express()
+
+const PORT= process.env.PORT || 5000
+
+
+
+app.listen(PORT ,()=>{
+    console.log("server started")
+})
+
+
+
+
+
+
+
+
+
+
+
